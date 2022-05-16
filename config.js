8389fa93b584d7c4ed0a37015fca621a8583f9cd
Josh Savvy <joshtee28@gmail.com>
2022-05-16 14:17:30 +0100

    implemented page visits counter

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const APP_URL = publicRuntimeConfig.APP_URL;
+export const JWT_SIGNATURE = publicRuntimeConfig.JWT_SIGNATURE;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
-import countapi from "countapi-js";
 import React, { useState, useEffect } from "react";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
-import { APP_URL } from "../config";
+import jwt from "jsonwebtoken";
+import { JWT_SIGNATURE } from "../config";
 
 const Home = () => {
   const [viewCounts, setViewCounts] = useState(0);
 
+  const countHandler = () => {
+    const visitCount = localStorage.getItem("page_view");
+    const visitCountUserId = localStorage.getItem("user_page_token");
+
+    if (!visitCountUserId) {
+      localStorage.setItem(
+        "user_page_token",
+        jwt.sign(
+          "User Visit Page Token",
+          JWT_SIGNATURE || process.env.JWT_SIGNATURE
+        )
+      );
+      visitCount = 18;
+      localStorage.setItem("page_view", 18);
+    } else if (visitCount) {
+      visitCount = Number(visitCount) + 1;
+      localStorage.setItem("page_view", visitCount);
+    }
+    setViewCounts(visitCount);
+  };
+
   useEffect(() => {
-    countapi
-      .visits(APP_URL || process.env.APP_URL)
-      .then((response) => {
-        setViewCounts(response.value);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    countHandler();
   }, []);
 
   return (
