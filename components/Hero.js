09e16d5103cfb9c3f4951d8aa1906d846621197a
Josh Savvy <joshtee28@gmail.com>
2022-05-18 01:37:53 +0100

    changed ui design

--- components/Hero.js
+++ components/Hero.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Hero = ({ viewCounts }) => {
   return (
-    <div className="">
-      <div className="flex">
-        <div>sjshdfdv</div>
-        <div>djkslf</div>
+    <div className="relative mt-16">
+      <div className="md:grid grid-cols-2">
+        <div className="mt-10">
+          
+        </div>
+        <div className="mt-4">djkslf</div>
       </div>
     </div>
   );
