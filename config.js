fb3390fb1c34c3019b61c343f179546197eb8b92
DevJosh <joshtee28@gmail.com>
2022-12-27 00:34:00 +0100

    Deleted config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-
-export const APP_URL = publicRuntimeConfig.APP_URL;
-export const JWT_SIGNATURE = publicRuntimeConfig.JWT_SIGNATURE;
